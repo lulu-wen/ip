33a8d2d625d75bd96a52104c6fd46b9cf9427335
lulu-wen <lulucoco0212@gmail.com>
2026-08-15 16:27:11 +0800

    Level 0. Rename, Greet, Exit

---
+++ src/main/java/Lumi.java
@@ -0,0 +1,18 @@
+public class Lumi {
+    public static void main(String[] args) {
+        String logo = " _    _   _ __  __ ___ \n"
+                + "| |  | | | |  \\/  |_ _|\n"
+                + "| |  | | | | |\\/| || | \n"
+                + "| |__| |_| | |  | || | \n"
+                + "|_____\\___/|_|  |_|___|\n";
+        String line = "____________________________________________________________";
+
+        System.out.println("Hello from\n" + logo);
+        System.out.println(line);
+        System.out.println(" Hello! I'm LUMI");
+        System.out.println(" What can I do for you?");
+        System.out.println(line);
+        System.out.println(" Bye. Hope to see you again soon!");
+        System.out.println(line);
+    }
+}
