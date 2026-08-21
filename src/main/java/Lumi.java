c0c1b686b6e5b0db8138166b1c3f57404d11583a
lulu-wen <lulucoco0212@gmail.com>
2026-08-21 20:50:51 +0800

    Level 1. Echo

--- src/main/java/Lumi.java
+++ src/main/java/Lumi.java
@@ -1,18 +1,43 @@
-public class Lumi {
-    public static void main(String[] args) {
-        String logo = " _    _   _ __  __ ___ \n"
-                + "| |  | | | |  \\/  |_ _|\n"
-                + "| |  | | | | |\\/| || | \n"
-                + "| |__| |_| | |  | || | \n"
-                + "|_____\\___/|_|  |_|___|\n";
-        String line = "____________________________________________________________";
+import java.util.Scanner;
 
-        System.out.println("Hello from\n" + logo);
+public class Lumi {
+    static String logo = " _    _   _ __  __ ___ \n"
+            + "| |  | | | |  \\/  |_ _|\n"
+            + "| |  | | | | |\\/| || | \n"
+            + "| |__| |_| | |  | || | \n"
+            + "|_____\\___/|_|  |_|___|\n";
+    static String line = "____________________________________________________________\n";
+    static String indent = "  ";
+    public static void speak(String... messages) {
         System.out.println(line);
-        System.out.println(" Hello! I'm LUMI");
-        System.out.println(" What can I do for you?");
+        for (String message : messages) {
+            // one speak, one line
+            System.out.println(indent + message);
+        }
         System.out.println(line);
-        System.out.println(" Bye. Hope to see you again soon!");
-        System.out.println(line);
+    }
+
+    public static void greet() {
+        System.out.println("Hello from");
+        System.out.println(logo + line);
+        System.out.println(indent + "Hello! I'm Lumi");
+        System.out.println(indent + "What can I do for you?");
+    }
+
+    public static void main(String[] args) {
+        greet();
+        System.out.print(line);
+        Scanner scanner = new Scanner(System.in);
+        while(scanner.hasNextLine()) {
+            String input = scanner.nextLine().trim();
+            if(input.toLowerCase().equals("bye")) {
+                break;
+            }
+            if (input.isEmpty()) {
+                continue;
+            }
+            speak(input);
+        }
+        speak("Bye. Hope to see you again soon!");
     }
 }
