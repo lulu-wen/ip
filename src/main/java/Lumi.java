239896eea3120f3dff3dc6709438aea7f83f6682
lulu-wen <lulucoco0212@gmail.com>
2026-08-22 10:39:43 +0800

    Level 2. Add, List

--- src/main/java/Lumi.java
+++ src/main/java/Lumi.java
@@ -1,4 +1,5 @@
 import java.util.Scanner;
+import java.util.ArrayList;
 
 public class Lumi {
     static String logo = " _    _   _ __  __ ___ \n"
@@ -8,6 +9,21 @@ public class Lumi {
             + "|_____\\___/|_|  |_|___|\n";
     static String line = "____________________________________________________________\n";
     static String indent = "  ";
+    static ArrayList<String> tasks = new ArrayList<>();
+
+    public static void addTask(String description) {
+        tasks.add(description);
+        speak("added: " + description);
+    }
+
+    public static void listTasks() {
+        String[] lines = new String[tasks.size()];
+        for (int i = 0; i < tasks.size(); i++) {
+            lines[i] = (i + 1) + ". " + tasks.get(i);
+        }
+        speak(lines);
+    }
+
     public static void speak(String... messages) {
         System.out.println(line);
         for (String message : messages) {
@@ -32,11 +48,11 @@ public class Lumi {
             String input = scanner.nextLine().trim();
             if(input.toLowerCase().equals("bye")) {
                 break;
+            } else if (input.toLowerCase().equals("list")) {
+                listTasks();
+            } else {
+                addTask(input);
             }
-            if (input.isEmpty()) {
-                continue;
-            }
-            speak(input);
         }
         speak("Bye. Hope to see you again soon!");
     }
